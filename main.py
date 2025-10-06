498fcea5775d28e69890b417cb999dd5ce5935db
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-06 14:49:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,39 +7,47 @@ import os
 
 os.system('color 1f')
 
-while True:
-
+try:
+    
     def menu():
-        
-        print('\n--------------')
-        print('| OFFICE 365 |')
-        print('--------------\n')
-        
-        os.system('pause')
-        
-        print('1 - Instalar Office 365')
-        print('2 - Sair')
-        
-        op = int(input('Selecione uma opção: '))
-        
-        os.system('cls')
-        
-        if op == 1:
-            
-            ...
-            
-        elif op == 2:
-            
-            break
-        
-        else:
-            
-            print('Favor repassar opção correta!')
-            os.system('pause')
-            menu()
-        
-    def ins_office():
-        
-        ...
+    
+        while True:
+                
+                print('\n--------------')
+                print('| OFFICE 365 |')
+                print('--------------\n')
+                
+                os.system('pause')
+                
+                print('1 - Instalar Office 365')
+                print('2 - Sair')
+                
+                op = int(input('Selecione uma opção: '))
+                
+                os.system('cls')
+                
+                if op == 1:
+                    
+                    ins_office
+                    
+                elif op == 2:
+                    
+                    print('Saindo...!')
+                    os.system('pause')
+                    break
+                
+                else:
+                    
+                    print('Favor repassar opção correta!')
+                    os.system('pause')
+                    menu()
+                
+                def ins_office():
+                
+                    ...
+
+except Exception  as error:
+    
+    print(f'{error.__class__.__name__}: {error}')
 
 menu()
