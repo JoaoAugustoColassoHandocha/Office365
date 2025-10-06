9f25ae0e6fdca4de61e58444af8f9b87ddbfc55b
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-06 14:21:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,7 @@ os.system('color 1f')
 
 while True:
 
-    print('\n******************************\n')
-
     
-
-    print('\n******************************\n')
 
 os.system('pause')
 os.system('cls')
