306b9137d20dcfa1cefd1c2bd2c7ef57866ce003
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-06 18:58:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,45 @@ import os
 
 os.system('color 1f')
 
-texto = 'Bem vindo ao instalador do Office 365!'
-largura = 30
+def menu():
+                
+    print('\n--------------')
+    print('| OFFICE 365 |')
+    print('--------------\n')
+                
+                print('1 - Instalar Office 365')
+                print('2 - Sair')
+                
+                op = int(input('\nSelecione uma opção: '))
+                
+                os.system('cls')
+                
+                if op == 1:
+                    
+                    ins_office()
+                    
+                elif op == 2:
+                    
+                    print('Saindo...!')
+                    os.system('pause')
+                    os.system('exit')
+                
+                else:
+                    
+                    print('Favor repassar opção correta!')
+                    os.system('pause')
+                    menu()
+                
+                def ins_office():
 
 try:
     
-    def menu():
-        
-        print(f'\n{texto:^{largura}}\n')
-        os.system('pause')
-        os.system('cls')
+    texto = 'Bem vindo ao instalador do Office 365!'
+    largura = 30
     
         while True:
+            
+            def menu():
                 
                 print('\n--------------')
                 print('| OFFICE 365 |')
@@ -47,14 +74,18 @@ try:
                     os.system('pause')
                     menu()
                 
-    def ins_office():
+                def ins_office():
                 
-        os.system('pause')
-        os.system('cls')
-        menu()
+    os.system('pause')
+    os.system('cls')
+    menu()
+        
+    print(f'\n{texto:^{largura}}\n')
+    os.system('pause')
+    os.system('cls')
+
+    menu()
 
 except Exception  as error:
     
     print(f'{error.__class__.__name__}: {error}')
-
-menu()
