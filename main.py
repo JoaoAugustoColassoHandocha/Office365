957e9f0eb656b31c717502f506fa53fb9ea1a3c6
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-06 14:24:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ os.system('color 1f')
 
 while True:
 
-    
+    def menu():
+        
+        print('\n--------------')
+        print('| OFFICE 365 |')
+        print('--------------\n')
 
 os.system('pause')
 os.system('cls')
