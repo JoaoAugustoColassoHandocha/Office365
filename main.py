2b6384d241fbe2388427722637c66d23d4974fa3
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-16 12:03:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 '''
-CUSTOM OFFICE INSTALLER IN PYTHON
 
-This project is a simplified installer for different versions of Microsoft Office, developed in Python. It was created to facilitate and centralize the Office installation process, using the Office Deployment Tool (ODT) and custom XML configuration files.
-
-The main objective is to provide a user-friendly command-line interface (CLI) that guides the user through choosing the desired Office version (Office 365 x64/x86, Office 2019 Enterprise, or Office 2021 Enterprise) and then automatically executes the appropriate installation command. This eliminates the need for the user to memorize or type lengthy ODT commands.
-
-This project is ideal for system administrators or corporate environments that need a quick and standardized way to install specific versions of Microsoft Office.
 
 '''
 
